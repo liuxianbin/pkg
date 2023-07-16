5813f00e2a34e45834ad7184e89ea1e8169421de
lau <lauj@foxmail.com>
2023-07-16 11:49:32 +0800

    feat: 校验

--- check/check.go
+++ check/check.go
@@ -5,13 +5,13 @@ import (
 	"reflect"
 )
 
-// Do 校验必填项 使用Tag：bind标识 msg描述错误信息
+// Do 校验必填项 使用Tag：binding标识 msg描述错误信息
 func Do(obj interface{}) (err error) {
 	v := reflect.Indirect(reflect.ValueOf(obj))
 	t := v.Type()
 	for i := 0; i < v.NumField(); i++ {
 		tag := t.Field(i).Tag
-		if _, ok := tag.Lookup("bind"); ok {
+		if _, ok := tag.Lookup("binding"); ok {
 			f := v.Field(i)
 			if !validate(f) {
 				return errors.New(tag.Get("msg"))
@@ -25,8 +25,19 @@ func validate(field reflect.Value) bool {
 	switch field.Type().Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		return field.Int() > 0
+	case reflect.Float32, reflect.Float64:
+		return field.Float() > 0
 	case reflect.String:
 		return len(field.String()) > 0
+	case reflect.Array, reflect.Slice:
+		result := true
+		for i := 0; i < field.Len(); i++ {
+			if !validate(field.Index(i)) {
+				result = false
+				break
+			}
+		}
+		return field.Len() > 0 && result
 	}
 	return true
 }
--- check/check_test.go
+++ check/check_test.go
@@ -6,8 +6,9 @@ import (
 
 func ExampleDo() {
 	type User struct {
-		Username string `json:"username" bind:"required" msg:"用户名不能为空"`
-		Age      int    `json:"age" bind:"required" msg:"年龄不能为空"`
+		Username string   `json:"username" binding:"required" msg:"用户名不能为空"`
+		Age      int      `json:"age" binding:"required" msg:"年龄不能为空"`
+		Tels     []string `binding:"required" msg:"手机不能为空"`
 	}
 	//Username不通过
 	user := User{
@@ -26,16 +27,29 @@ func ExampleDo() {
 		fmt.Println(err)
 	}
 
-	// 校验通过
+	//Tels不通过
 	user2 := User{
 		Username: "abc",
 		Age:      20,
+		Tels:     []string{""},
 	}
-	if err := Do(user2); err == nil {
+	if err := Do(user2); err != nil {
+		fmt.Println(err)
+	}
+
+	// 校验通过
+	user4 := User{
+		Username: "abc",
+		Age:      20,
+		Tels:     []string{"15888888888"},
+	}
+	if err := Do(user4); err == nil {
 		fmt.Println("pass")
 	}
+
 	// output:
 	// 用户名不能为空
 	// 年龄不能为空
+	// 手机不能为空
 	// pass
 }
