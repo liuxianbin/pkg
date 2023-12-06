c34d50c0127cf0169228d8776af06cd7b0d4e6ba
lauj@foxmail.com <lauj@foxmail.com>
2023-12-06 17:42:33 +0800

    feat: 新增鉴权失败和too many requests

--- errorx/errcode_common.go
+++ errorx/errcode_common.go
@@ -7,5 +7,7 @@ var (
 	BadRequest          = NewError(http.StatusBadRequest, 100001, "入参错误")
 	NotFound            = NewError(http.StatusNotFound, 100002, "资源不存在")
 	InternalServerError = NewError(http.StatusInternalServerError, 100003, "系统异常")
-	Unauthorized        = NewError(http.StatusUnauthorized, 100004, "鉴权失败")
+	Unauthorized        = NewError(http.StatusUnauthorized, 100004, "认证失败")
+	Forbidden           = NewError(http.StatusForbidden, 100005, "鉴权失败")
+	TooManyRequests     = NewError(http.StatusTooManyRequests, 100006, "Too Many Requests")
 )
