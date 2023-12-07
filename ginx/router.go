8eb26a64b7a64e0f53d0ad90b6e7cc210d033443
lauj@foxmail.com <lauj@foxmail.com>
2023-12-07 12:44:01 +0800

    feat: 包装路由

---
+++ ginx/router.go
@@ -0,0 +1,52 @@
+package ginx
+
+import "github.com/gin-gonic/gin"
+
+type Router struct {
+	*gin.Engine
+	excludePaths map[string][]string
+}
+
+func (r *Router) Exists(method string, path string) bool {
+	if paths, ok := r.excludePaths[method]; ok {
+		for _, p := range paths {
+			if p == path {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (r *Router) Exclude(method string, path string) {
+	r.excludePaths[method] = append(r.excludePaths[method], path)
+}
+
+func (r *Router) POST(relativePath string, handlers ...gin.HandlerFunc) (string, string) {
+	r.Engine.POST(relativePath, handlers...)
+	return "POST", relativePath
+}
+
+func (r *Router) GET(relativePath string, handlers ...gin.HandlerFunc) (string, string) {
+	r.Engine.GET(relativePath, handlers...)
+	return "GET", relativePath
+}
+
+func (r *Router) DELETE(relativePath string, handlers ...gin.HandlerFunc) (string, string) {
+	r.Engine.DELETE(relativePath, handlers...)
+	return "DELETE", relativePath
+}
+
+func (r *Router) PUT(relativePath string, handlers ...gin.HandlerFunc) (string, string) {
+	r.Engine.PUT(relativePath, handlers...)
+	return "PUT", relativePath
+}
+
+func NewRouter() *Router {
+	e := gin.New()
+	r := &Router{
+		Engine:       e,
+		excludePaths: make(map[string][]string),
+	}
+	return r
+}
